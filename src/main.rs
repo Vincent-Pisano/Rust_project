760e16cd50dc31cc58a12c32d8a2998561752d2e
Pravdin Maksym <maks111111112@gmail.com>
2022-08-11 15:50:03 -0400

    Added CLI input

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![allow(unused)]
 
+use std::io;
+
 mod account;
 
 use crate::account::{client::*, account_trait::*};
@@ -9,4 +11,15 @@ fn main() {
 
     let client: Client = AccountTrait::new(String::from("Hippie"), "De merde".to_owned());
     println!("{} {}", client.username, client.password);
+
+    let mut input = String::new();
+
+    print!("Enter your username:");
+
+    match io::stdin().read_line(&mut input) {
+        Ok(__) => {
+            println!("Your username is {}", input);
+        },
+        Err(e) => println!("Something went wrong: {}", e)
+    }
 }
